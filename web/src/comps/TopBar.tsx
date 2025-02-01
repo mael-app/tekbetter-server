d15561a940da4cf7e1eca6a436e251864172193a
Eliot <eliot.amanieu@epitech.eu>
2025-02-01 14:11:21 +0100

    update(dashboard): new navbar style

--- web/src/comps/TopBar.tsx
+++ web/src/comps/TopBar.tsx
@@ -17,7 +17,7 @@ function NavElement(props: { text: string, icon: any, link: string }) {
 
     return (
         <div
-            className={"flex flex-col rounded-2xl items-center justify-center cursor-pointer px-5 transition hover:bg-blue-800 hover:text-white " + (is_active ? "bg-blue-800 text-white font-bold" : "")}
+            className={"flex flex-col rounded-2xl items-center h-11 justify-center cursor-pointer px-5 transition hover:bg-blue-800 hover:text-white " + (is_active ? "bg-blue-800 text-white font-bold" : "")}
             onClick={() => navigate(props.link)}>
             <div className={"flex flex-row items-center justify-center"}>
                 <div>
@@ -59,7 +59,7 @@ function SyncStatus() {
     const gen_visual = (color: string, icon: any, text: string) => (
         <div className={"flex px-1.5 flex-row items-center rounded-full bg-blue-300 bg-opacity-20"}>
             <FontAwesomeIcon icon={icon} className={color} fontSize={"13px"}/>
-            <p className={"text-white text-xs my-1 ml-1 text-nowrap"}>Sync: {text}</p>
+            <p className={"text text-xs my-1 ml-1 text-nowrap"}>Sync: {text}</p>
         </div>
     );
 
@@ -90,35 +90,59 @@ function UserComp() {
 
     if (user === null) return null;
 
-
-    return <div className={"flex flex-row items-center gap-2"}>
-        <div className={"flex flex-row items-center mr-2"}>
-            <img
-                src={`${vars.backend_url}/api/global/picture/${user.login}`}
-                alt={"Profile Picture"}
-                className={"w-8 h-8 rounded-full object-cover mr-2"}
-            />
-
-            <p className={"text-white text-nowrap"}>{user?.name}</p>
+    return <div className={"flex flex-row h-full items-center shadow-lg px-3 rounded-2xl"}>
+        <img
+            src={`${vars.backend_url}/api/global/picture/${user.login}`}
+            alt={"Epitech"}
+            className={"w-11 h-11 ml-1 shadow rounded-full object-cover"}
+        />
+        <div className={"flex flex-col ml-2 items-start justify-center w-fit"}>
+            <p className={"font-bold"}>{user?.name}</p>
+            <SyncStatus/>
         </div>
-        <div title={"Logout"} className={"text-center bg-black bg-opacity-0 hover:bg-opacity-20 h-full w-10 p-2 cursor-pointer transition"} onClick={() => {
-            if (window.confirm("Are you sure you want to log out?")) {
-                localStorage.removeItem("auth");
-                window.location.reload();
-            }
-        }}>
-            <FontAwesomeIcon icon={faPowerOff} className={"text-red-500"}/>
+
+        <div title={"Logout"}
+             className={"text-center bg-black bg-opacity-0 hover:bg-gray-200 p-2 w-10 ml-2 h-10 rounded-full cursor-pointer transition"}
+             onClick={() => {
+                 if (window.confirm("Are you sure you want to log out?")) {
+                     localStorage.removeItem("auth");
+                     window.location.reload();
+                 }
+             }}>
+            <FontAwesomeIcon icon={faPowerOff} className={"text-red-500 cursor-pointer"} title={"Logout"}/>
         </div>
+        {/*<SyncStatus/>*/}
     </div>
+
+    // <div className={"flex flex-row items-center gap-2"}>
+    //        <div className={"flex flex-row items-center mr-2"}>
+    //            <img
+    //                src={`${vars.backend_url}/api/global/picture/${user.login}`}
+    //                alt={"Profile Picture"}
+    //                className={"w-8 h-8 rounded-full object-cover mr-2"}
+    //            />
+    //
+    //            <p className={"text-white text-nowrap"}>{user?.name}</p>
+    //        </div>
+    //        <div title={"Logout"}
+    //             className={"text-center bg-black bg-opacity-0 hover:bg-opacity-20 h-full w-10 p-2 cursor-pointer transition"}
+    //             onClick={() => {
+    //                 if (window.confirm("Are you sure you want to log out?")) {
+    //                     localStorage.removeItem("auth");
+    //                     window.location.reload();
+    //                 }
+    //             }}>
+    //            <FontAwesomeIcon icon={faPowerOff} className={"text-red-500"}/>
+    //        </div>
+    //    </div>
 }
 
 export default function TopBar(): React.ReactElement {
     return (
         <div
-            className={"flex flex-row justify-between items-center text-gray-700 overflow-x-auto p-3 scroll-container"}>
+            className={"flex flex-row justify-between h-20 items-center text-gray-700 py-2 overflow-x-auto scroll-container px-3"}>
 
-
-            <div className={"flex flex-row items-center mr-8 shadow-lg p-3 rounded-2xl"}>
+            <div className={"flex flex-row items-center"}>
                 <img
                     className={"w-9 ml-1 shadow rounded-full"}
                     src={require("../assets/tblogo.png")}
@@ -128,7 +152,7 @@ export default function TopBar(): React.ReactElement {
             </div>
 
 
-            <div className={"flex flex-row gap-2 rounded-2xl justify-start ml-2 h-full shadow-lg"}>
+            <div className={"flex flex-row gap-0.5 rounded-2xl justify-start ml-2 shadow-lg"}>
                 <NavElement text={"Moulinettes"} link={"/moulinettes"} icon={faGraduationCap}/>
                 <NavElement text={"Modules"} link={"/modules"} icon={faShareNodes}/>
                 <NavElement text={"Calendar"} link={"/calendar"} icon={faCalendarCheck}/>
@@ -138,15 +162,15 @@ export default function TopBar(): React.ReactElement {
 
             <UserComp/>
 
-            <div className={"flex flex-row items-center mr-8 shadow-lg p-3 rounded-2xl"}>
-                <img
-                    src={require("../assets/tblogo.png")}
-                    alt={"Epitech"}
-                    className={"w-9 ml-1 shadow rounded-full"}
-                />
-                <p className={"ml-1 mr-2 font-bold"}>Eliot Amanieu</p>
-                <SyncStatus/>
-            </div>
+            {/*<div className={"flex flex-row items-center mr-8 shadow-lg p-3 rounded-2xl"}>*/}
+            {/*    <img*/}
+            {/*        src={require("../assets/tblogo.png")}*/}
+            {/*        alt={"Epitech"}*/}
+            {/*        className={"w-9 ml-1 shadow rounded-full"}*/}
+            {/*    />*/}
+            {/*    <p className={"ml-1 mr-2 font-bold"}>Eliot Amanieu</p>*/}
+            {/*    <SyncStatus/>*/}
+            {/*</div>*/}
 
         </div>
     );
